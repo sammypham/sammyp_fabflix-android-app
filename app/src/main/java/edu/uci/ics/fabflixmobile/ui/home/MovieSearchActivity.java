7dcb32805b4ae2d8549cbcbdf1e35092fa4ffd77
sammypham <sammyphams@gmail.com>
2023-05-27 16:21:36 -0700

    Upload

---
+++ app/src/main/java/edu/uci/ics/fabflixmobile/ui/home/MovieSearchActivity.java
@@ -0,0 +1,21 @@
+package edu.uci.ics.fabflixmobile.ui.home;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import edu.uci.ics.fabflixmobile.R;
+
+import java.util.ArrayList;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+
+    }
+}
